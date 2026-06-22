6dae96c1708043b277c13abd4e63e94db422886e
Jaime Lopez-Merizalde <jaime.lopez@petdesk.com>
2026-06-22 18:46:06 -0400

    update monte carlo class

--- src/estimators/monte_carlo.py
+++ src/estimators/monte_carlo.py
@@ -2,8 +2,11 @@
 
 import numpy as np
 
-class build_estimator():
-    def build_estimator(self, func, **params): 
+class monte_carlo():
+    def __init__(self):
+        return None
+    
+    def estimator(self, func, **params): 
         mc_estimator = lambda data: sum(func(data)) / len(data)
         return mc_estimator
     
